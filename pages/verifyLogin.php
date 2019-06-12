e5b35d31e108c3156d2983b51445c99ec2a94fa0
honza <email@email.com>
2019-06-12 18:14:48 +0200

    update verifyLogin page

--- pages/verifyLogin.php
+++ pages/verifyLogin.php
@@ -2,10 +2,10 @@
 // Checks if user exists
 // if yes then set session values for current user
 	$password = hashPwd($_POST['password']);
-	if (verifyLogin($_POST['username'], $password))
+	if ($userdata = verifyLogin($_POST['email'], $password))
 	{
-		$_SESSION['username'] = $_POST['username'];
-		echo "succesful Login!";
+		$_SESSION['username'] = $userdata['username'];
+		echo "Welcome, " .$userdata['username'] ."!";
 		exit;
 	}
 	echo "Wrong password or username";
