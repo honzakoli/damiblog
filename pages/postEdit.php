10ad62333c6e58a0eeb170ff3e341bd3ea95b037
honza <email@email.com>
2019-06-14 14:49:12 +0200

    updated post edit page

--- pages/postEdit.php
+++ pages/postEdit.php
@@ -1,14 +1,9 @@
 <?php
-$headline = $_POST['postHeadline'];
-$body = $_POST['postBody'];
-$id = $_POST['postId'];
 
-// get current post data from database
 $statement = $database->prepare('SELECT * FROM post WHERE id = ?');
-$statement->bindValue(1, $id);
-$statement->execute();
-
-
+	$statement->bindValue(1,$_GET["id"], PDO::PARAM_INT);
+	$result = $statement->execute();
+	$row = $result->fetchArray(SQLITE3_ASSOC);
 ?>
 
 <!-- edit post form -->
@@ -23,15 +18,15 @@ $statement->execute();
 					<div class="form-group">
 						<!-- input headline -->
 						<label for="Headline">Headline</label>
-						<input type="text" class="form-control" name="headline" value="<?=$headline?>">
+						<input type="text" class="form-control" name="headline" value="<?=$row['headline']?>">
 					</div>
 					<div class="form-group">
 						<!-- input Body -->
 						<label for="Body">Body</label>
-						<input type="text" class="form-control" name="body" value="<?=$body?>">
+						<input type="text" class="form-control" name="body" value="<?=$row['body']?>">
 					</div>
 					<div class="form-group">
-						<input type="hidden" name="id" value="<?=$id?>">
+						<input type="hidden" name="id" value="<?=$row['id']?>">
 					</div>
 				<!-- submit button -->
 					<button type="submit" name="saveChanges" class="btn btn-dark">Save Changes</button>
